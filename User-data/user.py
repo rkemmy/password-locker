44728b15112ea1feecea6af389d430fe1fc191f9
rkemmy <rkemmy.rk69@gmail.com>
2018-04-02 21:44:56 +0300

    Create a test and function that saves user data to the user_list array

--- User-data/user.py
+++ User-data/user.py
@@ -5,7 +5,7 @@ class User:
 
     user_list = [] #Empty user list
 
-    def __init__(self, user_name, password):
+    def __init__(self, site_name, user_name, password):
         """
         __init__ method that helps us define properties for our objects.
 
@@ -14,5 +14,12 @@ class User:
             password : New password.
 
         """
+        self.site_name = site_name
         self.user_name = user_name
         self.password = password
+
+    def save_user(self):
+        """
+        save_user method saves user objects into user_list
+        """
+        User.user_list.append(self)
--- User-data/user_test.py
+++ User-data/user_test.py
@@ -15,3 +15,19 @@ class TestContact(unittest.TestCase):
         self.new_user = User("Facebook", "remmy", "34y8")
 
     def test_init(self):
+        """
+        test_init test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.new_user.site_name,"Facebook")
+        self.assertEqual(self.new_user.user_name,"remmy")
+        self.assertEqual(self.new_user.password,"34y8")
+
+    def test_save_user(self):
+        """test_save_user test case to test if the user object is saved into the user user_list
+        """
+        self.new_user.save_user() #saving the new users
+        self.assertEqual(len(User.user_list), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
