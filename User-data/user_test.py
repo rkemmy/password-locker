aa0f46a3cff2b608272d26842538962a423e6492
rkemmy <rkemmy.rk69@gmail.com>
2018-04-02 21:26:02 +0300

    create a set up method

--- User-data/user_test.py
+++ User-data/user_test.py
@@ -1,10 +1,17 @@
 import unittest
-from user import user
+from user import User
 
 class TestContact(unittest.TestCase):
     """
-    Test class that defines test cases for the user class behaviour
+    Test class that defines test cases for the user class behaviour.
 
     Args:
-    unittest.TestCase: TestCase class that helps in creating test cases
+    unittest.TestCase: TestCase class that helps in creating test cases.
     """
+    def setUp(self):
+        """
+        Set up method to run before each test cases.
+        """
+        self.new_user = User("Facebook", "remmy", "34y8")
+
+    def test_init(self):
