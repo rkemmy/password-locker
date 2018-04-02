c665d165185296fd297abcc3cad0fa1647eb643c
rkemmy <rkemmy.rk69@gmail.com>
2018-04-02 20:55:52 +0300

    Initialize the user class with the __init__ method and pass in arguments

---
+++ User-data/user.py
@@ -0,0 +1,18 @@
+class User:
+    """
+    Class that generates new instances of users
+    """
+
+    
+
+    def __init__(self, user_name, password):
+        """
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            user_name: New user user_name.
+            password : New password.
+
+        """
+        self.user_name = user_name
+        self.password = password
